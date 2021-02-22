14a6709ae3e7bbf34d12283f3816c59a6daa71a9
Элина Обухова <45429218+WriteWrote@users.noreply.github.com>
2021-02-22 19:49:41 +0300

    Task 1

--- main.py
+++ main.py
@@ -2,8 +2,7 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-import math
+from typing import List
 
 
 def print_hi(name):
@@ -11,24 +10,40 @@ def print_hi(name):
     print(f'Hi, {name}!')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
+def func(list: List[float]):
+    # map for counting numbers
+    map = {}
+
+    for currEl in list:
+        if currEl not in map:
+            map[currEl] = 0
+        map[currEl] += 1
+
+    # turning map into list of entries?
+    map = [(v, k) for v, k in map.items()]
+    map.sort(key=lambda k: (-k[1], k[0]))
+    # finally getting list as result
+
+    # why do not working "result = list()"?
+    # changes to "result = list"
+    result = []
+    for value in map:
+        for i in range(0, value[1]):
+            result.append(value[0])
+
+    return result
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    #    print_hi('PyCharm')
-    l:[0,1,2,3,4,5]
-    l[1] #1
-    l[0:2] #[0,1] aka l[:0]
-    l[2:] #[2,3,4,5]
-    l[-1] #[5]
-    l[::2] #[0,2,4]
-    l[0:11:2] #выбрать элементы с 0 по 10 включительно с шагом 2
-    l[1:-1] #выбрать все эл-ты, кроме 1 и последнего
-    l[::-1] #выбрать эл-ты реверсивно
-    n=3 # n!=0
-    l[n:] + l[n-1:] #?
-
-
-#    name = input("Hi! What are you called, Jake Sully?\n")
-#    print_hi(name)
-
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    str = None
+try:
+    # get input and trim spaces in the end
+    str = input('enter values:\n').strip()
+    # casting float
+    l = [float(x) for x in str.split(' ')]
+    # print(l)
+    result = func(l)
+    print(result)
+except ValueError:
+    print("Wrong type of arguments")
