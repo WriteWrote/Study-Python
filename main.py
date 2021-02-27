e70bb29a7ddc85524254222533f3c2e87bf6215c
Элина Обухова <45429218+WriteWrote@users.noreply.github.com>
2021-02-27 12:41:40 +0300

    Task2_checkpoint_readingFromFile

--- main.py
+++ main.py
@@ -3,13 +3,13 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-
+"""
 def check_array(input_array):
     checkpoint = len(input_array)
     for x in range(len(input_array)):
 
     return False
-
+"""
 
 if __name__ == '__main__':
     try:
@@ -18,11 +18,13 @@ if __name__ == '__main__':
         try:
             input_str = []
             for line in arr_file:
-                input_str.append(line.strip())
+                input_str.append(line.strip().split("\n")[0])
             # casting
-            input_array = [float(x) for x in str.split(' ')]
+            input_array = []
+            for line in input_str:
+                input_array.append([float(x) for x in line.split(' ')])
             # checking
-            print(check_array(input_array))
+            print(input_array)
         except Exception as e:
             print(e)
     except FileNotFoundError:
