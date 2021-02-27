cb57edcc0e01ce0c6a636f612af04daa130e598f
Элина Обухова <45429218+WriteWrote@users.noreply.github.com>
2021-02-27 12:52:52 +0300

    Task2_checkpoint_preparationForMainDeal

--- main.py
+++ main.py
@@ -3,29 +3,41 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-"""
+
 def check_array(input_array):
+    res = False
+    seq = []  # for sequence
+
     checkpoint = len(input_array)
     for x in range(len(input_array)):
+        res = True
+    # return tuple
+    return res, seq
 
-    return False
-"""
 
 if __name__ == '__main__':
     try:
         # read from file
-        arr_file = open("input.txt", "r")
+        inp_file = open("input.txt", "r")
+        inp_array = []
         try:
-            input_str = []
-            for line in arr_file:
-                input_str.append(line.strip().split("\n")[0])
+            inp_str = []
+            for line in inp_file:
+                inp_str.append(line.strip().split("\n")[0])
             # casting
-            input_array = []
-            for line in input_str:
-                input_array.append([float(x) for x in line.split(' ')])
+            for line in inp_str:
+                inp_array.append([float(x) for x in line.split(' ')])
             # checking
-            print(input_array)
+            print(inp_array)
         except Exception as e:
             print(e)
+        finally:
+            inp_file.close()
+
+        # the key element of task
+        result, seq = check_array(inp_array)
+        print(result, seq)
     except FileNotFoundError:
         print("No such file")
+    except Exception as e:
+        print(e)
