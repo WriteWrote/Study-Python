71e1976f5ce1d0fa248beb2212363de123f494ef
Элина Обухова <45429218+WriteWrote@users.noreply.github.com>
2021-02-27 12:28:53 +0300

    Task2_checkpoint

--- main.py
+++ main.py
@@ -2,48 +2,28 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-from typing import List
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}!')  # Press Ctrl+F8 to toggle the breakpoint.
+def check_array(input_array):
+    checkpoint = len(input_array)
+    for x in range(len(input_array)):
+
+    return False
 
 
-def func(list: List[float]):
-    # map for counting numbers
-    map = {}
-
-    for currEl in list:
-        if currEl not in map:
-            map[currEl] = 0
-        map[currEl] += 1
-
-    # turning map into list of entries?
-    map = [(v, k) for v, k in map.items()]
-    map.sort(key=lambda k: (-k[1], k[0]))
-    # finally getting list as result
-
-    # why do not working "result = list()"?
-    # changes to "result = list"
-    result = []
-    for value in map:
-        for i in range(0, value[1]):
-            result.append(value[0])
-
-    return result
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    str = None
-try:
-    # get input and trim spaces in the end
-    str = input('enter values:\n').strip()
-    # casting float
-    l = [float(x) for x in str.split(' ')]
-    # print(l)
-    result = func(l)
-    print(result)
-except ValueError:
-    print("Wrong type of arguments")
+    try:
+        # read from file
+        arr_file = open("input.txt", "r")
+        try:
+            input_str = []
+            for line in arr_file:
+                input_str.append(line.strip())
+            # casting
+            input_array = [float(x) for x in str.split(' ')]
+            # checking
+            print(check_array(input_array))
+        except Exception as e:
+            print(e)
+    except FileNotFoundError:
+        print("No such file")
