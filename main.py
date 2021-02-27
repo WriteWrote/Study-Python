a9f3b31d2def186b48e63cf6d5cefe9bfae4a09e
Элина Обухова <45429218+WriteWrote@users.noreply.github.com>
2021-02-27 12:28:14 +0300

    Task2_checkpoint

--- main.py
+++ main.py
@@ -3,32 +3,27 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-import math
+
+def check_array(input_array):
+    checkpoint = len(input_array)
+    for x in range(len(input_array)):
+
+    return False
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}!')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    #    print_hi('PyCharm')
-    l:[0,1,2,3,4,5]
-    l[1] #1
-    l[0:2] #[0,1] aka l[:0]
-    l[2:] #[2,3,4,5]
-    l[-1] #[5]
-    l[::2] #[0,2,4]
-    l[0:11:2] #выбрать элементы с 0 по 10 включительно с шагом 2
-    l[1:-1] #выбрать все эл-ты, кроме 1 и последнего
-    l[::-1] #выбрать эл-ты реверсивно
-    n=3 # n!=0
-    l[n:] + l[n-1:] #?
-
-
-#    name = input("Hi! What are you called, Jake Sully?\n")
-#    print_hi(name)
-
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    try:
+        # read from file
+        arr_file = open("input.txt", "r")
+        try:
+            input_str = []
+            for line in arr_file:
+                input_str.append(line.strip())
+            # casting
+            input_array = [float(x) for x in str.split(' ')]
+            # checking
+            print(check_array(input_array))
+        except Exception as e:
+            print(e)
+    except FileNotFoundError:
+        print("No such file")
