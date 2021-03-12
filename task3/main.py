9277f021a7c04702be09cca4f393a0e2e96d3bf6
Элина Обухова <45429218+WriteWrote@users.noreply.github.com>
2021-03-12 15:07:22 +0300

    Task3_27_initial_commit
    Made some empty voids for future, also constructed a new class for Student. Next will be doing file-reading list of students and pulling info from strings to form objects of Student-class

---
+++ task3/main.py
@@ -0,0 +1,11 @@
+def read_from_file(name: str) -> list:
+    """Reading from file to list of string"""
+    pass
+
+
+def _cut_grades(inp_list: list) -> list:
+    """separating input list of students to several lists according to the grades"""
+    pass
+
+
+if __name__ == '__main__':
